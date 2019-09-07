a15c29a856f7bd15356f8deb3fa3e12c3f4ba84b
Vaisakh Murali <vaisakhmurali@gmail.com>
2019-09-07 06:36:53 +0200

    eva: Drop vendorsetup.sh
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- vendorsetup.sh
+++
@@ -1,8 +0,0 @@
-. "$(gettop)/vendor/eva/build/envsetup.sh"
-
-eva_device_combos
-
-# SDClang Environment Variables
-export SDCLANG_AE_CONFIG=vendor/eva/sdclang/sdclangAE.json
-export SDCLANG_CONFIG=vendor/eva/sdclang/sdclang.json
-export SDCLANG_SA_ENABLED=false
