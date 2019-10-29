SlimRoms/vendor_slim

Add generic gzosp targets to lunch choices
Change-Id: I80d77d7cdbdfa156e951003a218ba8a58d51b3d4

build/target/product/AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,3 +18,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/gzosp_arm.mk \
     $(LOCAL_DIR)/gzosp_arm64.mk \
     $(LOCAL_DIR)/gzosp_x86.mk
+
+COMMON_LUNCH_CHOICES := \
+    gzosp_arm-userdebug \
+    gzosp_arm64-userdebug \
+    gzosp_x86-userdebug

ProjektEva/platform_vendor_eva

Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

build/target/product/AndroidProducts.mk
@@ -18,3 +18,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/eva_arm.mk \
     $(LOCAL_DIR)/eva_arm64.mk \
     $(LOCAL_DIR)/eva_x86.mk
+
+COMMON_LUNCH_CHOICES := \
+    eva_arm-userdebug \
+    eva_arm64-userdebug \
+    eva_x86-userdebug